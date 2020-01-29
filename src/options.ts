555849f31c3242e5106e5eea2c9ac3751712e0a7
John Long <johnyscrazy@gmail.com>
2020-01-29 23:58:02 +0100

    Release 0.0.4

--- src/options.ts
+++ src/options.ts
@@ -1,17 +1,45 @@
-export interface PieOptions {
-    barColor: string,
-    trackColor: string,
-    scaleColor: string,
-    scaleLength: number,
-    lineCap: string,
-    lineWidth: number,
-    size: number,
-    rotate: number,
-    animate: number,
-    renderer: string | null,
-    easing(_x:number, t:number, b:number, c:number, d:number) : number,
-    onStart(_from:number, _to:number) : void,
-    onStep(_from:number, _to:number, _currentValue:number) : void,
-    onStop(_from:number, _to:number) : void
+export interface EasyPieOptions {
+    barColor?: string;
+    trackColor?: string;
+    scaleColor?: string;
+    scaleLength?: number;
+    lineCap?: string;
+    lineWidth?: number;
+    size?: number;
+    rotate?: number;
+    animate?: { duration: number, enabled: boolean };
+    renderer?: string | null;
+    easing?: CallableFunction;
+    onStart?: CallableFunction;
+    onStep?: CallableFunction;
+    onStop?: CallableFunction;
 }
 
+export const defaultOptions: Required<EasyPieOptions> = {
+    barColor: '#ef1e25',
+    trackColor: '#f9f9f9',
+    scaleColor: '#dfe0e0',
+    scaleLength: 5,
+    lineCap: 'round',
+    lineWidth: 3,
+    size: 110,
+    rotate: 0,
+    animate: { duration: 1000, enabled: true },
+    renderer: null,
+    easing: (_x: number, t: number, b: number, c: number, d: number) => {
+        t = t / (d / 2);
+        if (t < 1) {
+            return c / 2 * t * t + b;
+        }
+        return -c / 2 * ((--t) * (t - 2) - 1) + b;
+    },
+    onStart: (_from: number, _to: number) => {
+        return;
+    },
+    onStep: (_from: number, _to: number, _currentValue: number) => {
+        return;
+    },
+    onStop: (_from: number, _to: number) => {
+        return;
+    }
+};
